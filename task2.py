f46e9de1aad3e4850049b08ff7c1b5f1206e2b2a
Wayupkg <Adikgk232323@gmail.com>
2020-02-16 19:51:38 +0600

    Task2

---
+++ task2.py
@@ -0,0 +1,9 @@
+def Task2():
+    number_students = int(input("Введите количество студентов - "))
+    students = [input(f"Введите имя {namber} студентов - ") for i in range(namber)]
+    index = len(spisok)
+
+    number_apple = int(input("Введите количество яблок - "))
+    if index > apple 
+
+Task2()
